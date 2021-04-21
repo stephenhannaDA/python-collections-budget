db33c74b1f9135736855c2c81e291e60276cef0b
stephenhannaDA <73045342+stephenhannaDA@users.noreply.github.com>
2021-04-21 02:37:32 -0700

    Module5

--- budget/Expense.py
+++ budget/Expense.py
@@ -40,3 +40,19 @@ class Expenses():
                     unnecessary_expenses.add(i)
             
             return [necessary_expenses, food_expenses, unnecessary_expenses]
+
+    def categorize_set_comprehension(self):
+        necessary_expenses = {x for x in self.list
+        if x.category == 'Phone' or x.category == 'Auto and Gas' or x.category == 'Classes' or x.category == 'Utilities' or x.category == 'Mortgage'}
+        food_expenses = {x for x in self.list if x.category == 'Groceries' or x.category == 'Eating Out'}
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+        return necessary_expenses, food_expenses, unnecessary_expenses
+
+    divided_set_comp = expenses.categorize_set_comprehension()
+
+    if divided_set_comp != divided_for_loop:
+        print('Sets are NOT equal by == test')
+
+    for a,b in zip(divided_for_loop, divided_set_comp):
+        if not (a.issubset(b) and b.issubset(a)):
+            print("Sets are NOT equal by subset test")
