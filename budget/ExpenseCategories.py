43ca88807bfad92b4e22002025a037172076c81a
stephenhannaDA <73045342+stephenhannaDA@users.noreply.github.com>
2021-04-21 03:06:48 -0700

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -36,16 +36,12 @@ expenses.read_expenses('data/spending_data.csv')
 		))
 
     fig, ax = plt.subplots()
-
     labels = ['Necessary', 'Food', 'Unnecessary']
-
     divided_expenses_sum = []
-
     for category_exps in divided_set_comp:
     	divided_expenses_sum.append(sum([x.amount for x in category_exps]))
 
     ax.pie(divided_expenses_sum, labels = labels, autopct = '%1.1f%%')
-
     plt.show()
 
 
