77f93e174fe70b715a58665964d57bf33c36c0ab
stephenhannaDA <73045342+stephenhannaDA@users.noreply.github.com>
2021-04-21 03:01:06 -0700

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -15,16 +15,18 @@ def main():
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
 
-    print(timeit.timeit(stmt = "expenses.categorize_for_loop()", setup='''
-    	from . import Expense
-		expenses = Expense.Expenses()
-		expenses.read_expenses('data/spending_data.csv')
+    print(timeit.timeit(stmt = "expenses.categorize_for_loop()", 
+    setup='''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
 		''', number=100000, globals=globals()))
 
-    print(timeit.timeit(stmt = "expenses.categorize_set_comprehension()", setup='''
-    	from . import Expense
-		expenses = Expense.Expenses()
-		expenses.read_expenses('data/spending_data.csv')
+    print(timeit.timeit(stmt = "expenses.categorize_set_comprehension()", 
+    setup='''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
 		''', number=100000, globals=globals()))
 
     fig, ax = plt.subplots()
